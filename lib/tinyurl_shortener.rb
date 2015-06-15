ad21a0694bb9164b1260c7684c68d328e545b923
hrs <hrs113355@gmail.com>
2015-06-15 12:41:28 +0800

    fix issue of detecting url redirection when expanding tinyurl

--- lib/tinyurl_shortener.rb
+++ lib/tinyurl_shortener.rb
@@ -7,5 +7,22 @@ class TinyurlShortener
   def self.shorten(long_url)
     RestClient.get 'http://tinyurl.com/api-create.php', {params: {url: long_url}}
   end
+
+  def self.expand(short_url)
+    response = RestClient.get(short_url)
+
+    # there are some ways that tinyurl redirects
+    #   * using 301 location redirection
+    #   * using content refresh redirection
+    #
+    #   when we cannot detect the redirecting way, use current url for results
+    if response.history.first && response.history.first.headers
+      response.history.first.headers[:location]
+    elsif ! (match = response.body.response.body.scan(/content=\\"1; url=(.*?)\\"/)).empty?
+      match.last.first
+    else
+      response.request.url
+    end
+  end
 end
 
